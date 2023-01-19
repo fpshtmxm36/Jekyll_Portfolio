bc32b2a4bc69f14dff9d82df3f557248e76aef8b
fpshtmxm36 <fpshtmxm36@gmail.com>
2023-01-19 18:21:49 +0900

    230119_다크모드추가
    * 토글형 다크모드 추가
    * 구성 변경 및 Career 양식 수정
    * 할 일 갱신

--- js/creative.js
+++ js/creative.js
@@ -15,6 +15,18 @@
         }, 1250, 'easeOutQuart');
         event.preventDefault();
     });
+    
+    $('#mode-switch').bind('click', function(event) {
+        if(document.body.dataset.theme == "light"){
+            $('#switch').addClass("fa-moon-o");
+            $('#switch').removeClass("fa-sun-o");
+            document.body.dataset.theme = "dark";
+        }else{
+            $('#switch').addClass("fa-sun-o");
+            $('#switch').removeClass("fa-moon-o");
+            document.body.dataset.theme = "light";
+        }
+    });
 
     // Highlight the top nav as scrolling occurs
     $('body').scrollspy({
